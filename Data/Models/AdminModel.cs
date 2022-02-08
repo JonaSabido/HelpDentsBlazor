e7bd13e253823d4e5162c19543a6d6126f89e0c4
AbrahamHerrera011 <AbrahamHerrera0001@gmail.com>
2022-02-07 23:21:10 -0600

    Agregar archivos de proyecto.

---
+++ Data/Models/AdminModel.cs
@@ -0,0 +1,14 @@
+﻿using HelpDentsProyecto.Data;
+using HelpDentsProyecto.Data.Models;
+using HelpDentsProyecto.Pages;
+namespace HelpDentsProyecto.Data.Models
+{
+    public class AdminModel
+    {
+        public string? Nombres { get; set; }
+        public string? Apellidos { get; set; }
+        public string? Correo { get; set; }
+        public string? Contraseña { get; set; }
+
+    }
+}
