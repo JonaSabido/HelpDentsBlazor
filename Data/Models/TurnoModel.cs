314a6d5685aef486a643c20fbe4484fc645d9dcd
AbrahamHerrera011 <AbrahamHerrera0001@gmail.com>
2022-02-08 00:21:24 -0600

    Cambios nuevos

---
+++ Data/Models/TurnoModel.cs
@@ -0,0 +1,7 @@
+﻿namespace HelpDentsProyecto.Data.Models
+{
+    public class TurnoModel
+    {
+        public string? Nombre { get; set; }
+    }
+}
---
+++ Data/Models/UsuarioModel.cs
@@ -0,0 +1,12 @@
+﻿namespace HelpDentsProyecto.Data.Models
+{
+    public class UsuarioModel
+    {
+        public string? Nombres { get; set; }
+        public string? Apellidos { get; set; }
+        public string? Correo { get; set; }
+        public string? Contraseña { get; set; }
+
+
+    }
+}
