22b7d10640534e7ef026be9a2f78e178f94e9221
JonaSabido <jona_sabido@hotmail.com>
2022-03-08 00:14:18 -0600

    update

---
+++ Data/Models/AsesorHasComentarioModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HelpDentsProyecto.Data.Models
+{
+    public class AsesorHasComentarioModel
+    {
+        public int Id { get; set; }
+        public int Asesor_idAsesor { get; set; }
+        public int Comentario_IdComentario { get; set; }
+    }
+}
---
+++ Data/Models/AsesorHasEspecialidadModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HelpDentsProyecto.Data.Models
+{
+    public class AsesorHasEspecialidadModel
+    {
+        public int Id {get; set;}
+        public int Asesor_idAsesor { get; set; }
+        public int Especialidad_idEspecialidad { get; set; }
+    }
+}
