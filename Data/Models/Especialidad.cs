ec44a3710729bdbe42d31a1c4fafe17309d6829f
JonaSabido <jona_sabido@hotmail.com>
2022-03-12 03:05:04 -0600

    tengo sueño

--- Data/Models/Especialidad.cs
+++ Data/Models/Especialidad.cs
@@ -1,8 +1,10 @@
+
 namespace HelpDentsProyecto.Data.Models
 {
     public class EspecialidadModel
     {
         public int Id { get; set; }
+
         public string? Nombre { get; set; }
     }
 }
