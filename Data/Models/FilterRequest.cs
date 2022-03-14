c5fe42f280ff5bc43683a7b937b02175d6faa204
JonaSabido <jona_sabido@hotmail.com>
2022-03-14 01:51:01 -0600

    tengo sueño

---
+++ Data/Models/FilterRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace HelpDentsProyecto.Data.Models
+{
+    public class FilterRequest
+    {
+        public int? idEspecialidad { get; set; }
+        public int? idTurno { get; set; }
+        public double? Costo { get; set; }
+    }
+}
