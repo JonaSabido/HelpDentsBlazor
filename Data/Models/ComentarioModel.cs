2f5a89fd27355fd029cd6b0fa5c24f3842351b92
JonaSabido <jona_sabido@hotmail.com>
2022-03-26 00:56:13 -0600

    Modulo solicitud agregado

--- Data/Models/ComentarioModel.cs
+++ Data/Models/ComentarioModel.cs
@@ -3,6 +3,7 @@ namespace HelpDentsProyecto.Data.Models
     public class ComentarioModel
     {
         public int Id { get; set; }
+        public int UsuarioId { get; set; }
         public string? Nombres{get; set;}
         public string? Apellidos{get; set;}
         public string? Descripcion{get; set;}
---
+++ Data/Models/SolicitudModel.cs
@@ -0,0 +1,22 @@
+﻿namespace HelpDentsProyecto.Data.Models
+{
+    public class SolicitudModel
+    {
+        public int UsuarioIdUsuario { get; set; }
+        public string? Especialidad1 { get; set; }
+        public string? Especialidad2 { get; set; }
+        public string? Especialidad3 { get; set; }
+        public string? Turno1 { get; set; }
+        public string? Turno2 { get; set; }
+        public string? Turno3 { get; set; }
+        public double? Costo { get; set; }
+        public string? Telefono { get; set; }
+        public string? Descripcion { get; set; }
+        public string? Facebook { get; set; }
+        public string? Instagram { get; set; }
+        public string? Linkendin { get; set; }
+        public string? YouTube { get; set; }
+        public string? Tipo { get; set; }
+        public string? Estado { get; set; }
+    }
+}
